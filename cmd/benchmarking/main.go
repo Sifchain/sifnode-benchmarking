15e7d8d8a23482f756748fac01dab39774e5d45a
EddCS <edd@correct.software>
2020-12-07 11:46:33 +0000

    Rename folder to follow Golang conventions

--- cmd/benchmarking/main.go
+++ cmd/benchmarking/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"github.com/Sifchain/sifnode-benchmarking"
 	"fmt"
 )
 
 func main() {
 	fmt.Printf("Hello, world!")
-	generator.Create("1.tx")
+	// generator.Create("1.tx")
 }
